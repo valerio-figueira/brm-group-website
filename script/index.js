65cbc78f98ae52fab2af4842c51274477c4e90be
valerio-figueira <j.valerio.figueira@gmail.com>
2023-01-29 12:27:23 -0300

    new images, converted videos, footer content and css update

--- script/index.js
+++ script/index.js
@@ -33,8 +33,6 @@ function createFiveStarScore(container){
 
 if(path.match("/index.html")){
     setIntroductionText();
-
-
 }
 
 
@@ -42,6 +40,21 @@ function setIntroductionText(){
     const container = document.querySelectorAll(".about-container .box");
 
     container[0].children[1].innerHTML = `
-    Honestidade, ética e respeito são princípios presentes na essência da BRM Group. Por este motivo, é fundamental que todos estejamos conscientes dos compromissos acordados nas leis anticorrupção e fomento de padrões éticos nas diferentes áreas de atuação. O cumprimento das diretrizes, valores e conduta exigida de todos os colaboradores, diretores, independente de sua posição na hierarquia organizacional. Graças a estas medidas, a BRM Group contribui para a criação de um ambiente social mais ético e a promoção de uma cultura de transparência.
+    A BRM Group foi criada pela necessidade de estruturar nossas <span class="destaque">soluções digitais</span> desenvolvidas para os clientes. A criação deste grupo forma um sólido conjunto de empresas que prezam pela <span class="destaque">qualidade dos serviços</span> e <span class="destaque">suporte ao cliente</span>. Garantimos a entrega de serviços individualizados e personalizados para cada tipo de organização. Nossa equipe fornece soluções digitais que trazem melhor experiência ao usuário. Somos focados em nossos clientes, no suporte e na qualidade de serviços prestados. Conte com a BRM Group para o sucesso de seu negócio.
     `;
+
+    container[1].children[1].innerHTML = `
+    Somos formados por princípios de integridade, ética e respeito ao ser humano. Por este motivo, estamos todos compromissados com estes valores. Nossos serviços são reconhecidos pela <span class="destaque">qualidade</span>, <span class="destaque">segurança</span> e <span class="destaque">agilidade</span> ao cliente. A BRM Group é composta por especialistas renomados na área de tecnologia, soluções digitais e contabilidade. Desenvolvemos aplicações que dão segurança e que supram aos interesses de nossos clientes.
+    `;
+}
+
+
+
+
+setCopyright()
+function setCopyright(){
+    const container = document.querySelector(".copyright");
+    const year = new Date().getFullYear();
+
+    container.innerHTML = `BRM Group - Direitos autorais &copy; ${year}`;
 }
