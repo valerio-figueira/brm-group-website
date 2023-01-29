50c2d14a62df44a0cb9fe0167bedca073daf16ce
valerio-figueira <j.valerio.figueira@gmail.com>
2023-01-29 19:07:21 -0300

    new update

--- script/index.js
+++ script/index.js
@@ -1,6 +1,26 @@
+setResponsiveNavbar();
 
+function setResponsiveNavbar(){
+    const openNav = document.querySelector(".mobile-btn");
+    const closeNav = document.querySelector(".close-btn");
+    const nav = document.querySelector(".navbar");
 
+    openNav.addEventListener("click", () => {
+        addConditionalNavbar(nav);
+    })
 
+    closeNav.addEventListener("click", () => {
+        addConditionalNavbar(nav);
+    })
+}
+
+function addConditionalNavbar(nav){
+    if(!nav.matches(".mobile")){
+        nav.classList.add("mobile");
+    } else{
+        nav.classList.remove("mobile");
+    }
+}
 
 
 
@@ -97,5 +117,3 @@ function createAnchor(string, url){
 
     return anchor;
 }
-
-
