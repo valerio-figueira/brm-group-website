cda08565140927adf32131d675378f0c0acba418
valerio-figueira <j.valerio.figueira@gmail.com>
2023-02-01 22:37:59 -0300

    fixing module bug

--- script/index.js
+++ script/index.js
@@ -169,12 +169,37 @@ if(document.querySelector("#contact")){
         let mail = new FormData(form);
 
         // SEND ELETRONIC MAIL
-        //sendMail(mail);
+        sendMail(mail);
     })
 
+    async function sendMail(mail){
+        const messageTag = document.querySelector('#contact-form .alert-msg');
+        // REMOVE MESSAGE IF IT ALREADY EXISTS
+        if(messageTag){
+            messageTag.remove();
+        }
+    
+        await fetch("http://localhost:3000/", {
+            method: "post",
+            body: mail
+        }).then((response) => {
+            const message = createMessageTag("Sua mensagem foi enviada!", "alert-msg");
+    
+            if(!document.querySelector('#contact-form .alert-msg')){
+                form.appendChild(message);
+                form.children[0].after(message);
+            }
+        })
+    }
 
-
+    
 }
 
 
+function createMessageTag(text, messageType){
+    const p = document.createElement('p');
+    p.className = messageType;
+    p.innerHTML = text;
 
+    return p;
+}
