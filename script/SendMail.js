0dcfbf0224fc526d4e63bfa44766f670d7a829e0
valerio-figueira <j.valerio.figueira@gmail.com>
2023-06-21 00:26:19 -0300

    refactor: make code clean

--- script/SendMail.js
+++ script/SendMail.js
@@ -1,17 +1,28 @@
 import Loader from "./Loader.js";
 import MailMessage from "./MailMessage.js";
 
-export default async function sendMail(mail){
+export default async function sendMail(mail) {
     Loader.run();
     MailMessage.deleteMessage();
 
-    await fetch("http://localhost:3000/", {
+    const body = {
+        nome: mail.get('nome'),
+        email: mail.get('email'),
+        assunto: mail.get('assunto'),
+        message: mail.get('message'),
+    }
+
+    await fetch("http://localhost:3000/send", {
         method: "POST",
-        body: mail
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include',
+        body: JSON.stringify(body)
     }).then((response) => {
+        console.log(response)
         MailMessage.createMessage("Sua mensagem foi enviada!", "alert-msg")
         Loader.stop();
     }).catch(error => {
+        console.error(error);
         MailMessage.createMessage("Falha na conexão", "error-msg")
         Loader.stop();
     })
