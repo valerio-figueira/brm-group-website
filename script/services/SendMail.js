4719ecde08315703be81f5cd41a1543f0dd99a57
valerio-figueira <j.valerio.figueira@gmail.com>
2023-06-25 09:57:30 -0300

    :recycle: update functions

--- script/services/SendMail.js
+++ script/services/SendMail.js
@@ -1,39 +1,35 @@
 import Loader from "../Utils/Loader.js";
 import MailMessage from "./CreateMailMessage.js";
 
-export default async function sendMail(mail) {
+export default async function sendMail(formData) {
     Loader.run();
     MailMessage.deleteMessage();
 
-    const body = {
-        name: mail.get('name'),
-        email: mail.get('email'),
-        subject: mail.get('subject'),
-        message: mail.get('message'),
-        recaptchaResponse: mail.get('g-recaptcha-response')
-    }
+    recaptcha = formData.get('recaptchaResponse');
 
-    if (body.recaptchaResponse.length === 0) {
+    if (recaptcha.length === 0) {
         MailMessage.createMessage("Marque o CAPTCHA antes de enviar sua mensagem", "warning-msg");
         Loader.stop();
     } else {
-        await fetch("https://brm-group-serverside.vercel.app/send", {
-            method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-            credentials: 'include',
-            body: JSON.stringify(body)
-        })
-            .then((response) => response.json())
-            .then(response => {
-                if (response.message) MailMessage.createMessage(response.message, "alert-msg");
-                if (response.error) MailMessage.createMessage(response.error, "error-msg");
-
-                Loader.stop();
-            })
-            .catch(error => {
-                console.error(error);
-                MailMessage.createMessage("Falha na conexão", "error-msg")
-                Loader.stop();
-            })
+        await createRequest(formData)
     }
+}
+
+async function createRequest(formData) {
+    return fetch("http://localhost:9292/contact", {
+        method: "POST",
+        body: formData
+    })
+        .then((response) => response.json())
+        .then(response => {
+            if (response.message) MailMessage.createMessage(response.message, "alert-msg");
+            if (response.error) MailMessage.createMessage(response.error, "error-msg");
+
+            Loader.stop();
+        })
+        .catch(error => {
+            console.error(error);
+            MailMessage.createMessage("Falha na conexão", "error-msg")
+            Loader.stop();
+        })
 }
